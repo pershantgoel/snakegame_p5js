532d6241912906e0092d531fe8cb7eb1ff2a064c
Pershant Goel <pershantgoel@gmail.com>
2018-05-12 02:05:18 +0530

    dev mode

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
+<<<<<<< HEAD
+var s;
+var scl=20; //var which is size of a grid
+var food;
+
+
+function setup() {
+    createCanvas(700,500);  //width x height
+    s= new Snake();  //making an object of Snake constructor fn
+    frameRate(10);
+    pickLocation();      
+}
+
+function pickLocation(){
+    var cols = floor(width/scl);    //floor is for whole number
+    var rows = floor(height/scl);
+    food= createVector( floor(random(cols)) , floor(random(rows)) );     //createVector([x], [y], [z]) will create random x n y
+    food.mult(scl); //contains predefine mult fn
+}
+
+// function mousePressed(){
+//     s.total++;
+// }
+
+function draw(){
+    background(51); //makes background black
+    s.death();
+    s.update();
+    s.show();
+    fill(255,0,100);    //coloring the food
+    rect(food.x, food.y, scl,scl);  // making food shape same block(rect) of snake  //scl is jst like a block in the grid 
+    if(s.eat(food)){
+        pickLocation();    //sets new location for food
+    }
+}
+
+function keyPressed(){
+    if(keyCode==UP_ARROW){  // keyCode is inbuilt var in p5
+        s.dir(0 , -1);      // 0 along x axis and -1 is up along y axis
+    } 
+    else if (keyCode==DOWN_ARROW){
+        s.dir(0,1);
+    }
+    else if (keyCode==RIGHT_ARROW){
+        s.dir(1,0);
+    }
+    else if (keyCode==LEFT_ARROW){
+        s.dir(-1,0);
+    }
+}
+
+=======
 var s;
 var scl=20; //var which is size of a grid
 var food;
@@ -42,3 +94,4 @@ function keyPressed(){
     }
 }
 
+>>>>>>> d28b8c0aff777bfccb08b526c2dd40b39d7329b4
--- snake.js
+++ snake.js
@@ -1,3 +1,76 @@
+<<<<<<< HEAD
+function Snake(){  //it is a class or constructor function (class--> thats why name in capital)
+                    //this function is working like a contructor of a class
+this.x=0;
+this.y=0;
+this.xspeed=1;
+this.yspeed=0;
+this.total=0;   //length of snake(saanp) , initially zero bcoz bydefault it contains its initial position
+this.tail=[];
+
+this.eat=(pos)=>{
+    var d=dist(this.x , this.y , pos.x ,pos.y);  //pos.x&y is position of food n this.x&y is of snake(saanp)
+    if(d<1){
+        this.total++;
+        return true;
+    }
+    else{ return false;}
+}
+
+this.dir=(x,y)=>{
+    this.xspeed=x;
+    this.yspeed=y;
+}
+
+this.death=()=>{        // dying the snake if it encounters with its own body
+    for(let i=0 ; i<this.tail.length; i++){
+        var pos = this.tail[i];
+        var d = dist(this.x , this.y, pos.x, pos.y);
+        if(d<1){
+            this.total=0;
+            this.tail=[];
+            //document.querySelector("h3").innerHTML="Game Over";
+            alert("next game will start");
+        }
+    }
+    
+}
+
+this.update= ()=>{
+    if(this.total === this.tail.length){    //no food is been eaten
+        for(let i=0 ; i<this.tail.length-1 ; i++){  //in this loop the tail of the snake will move with snake
+            this.tail[i] = this.tail[i+1];
+        }
+    }
+    
+    this.tail[this.total-1] = createVector(this.x , this.y);
+
+// for (let i=0; i<this.total-1 ;i++){    //this loop will add an element(snake body part at the end)
+//     this.tail[i] = this.tail[i+1]; 
+// }
+// this.tail[this.total-1] = createVector(this.x , this.y);    //????????????what is this doing
+this.x += (this.xspeed*scl);
+this.y += (this.yspeed*scl);
+
+this.x=constrain(this.x, 0, width-scl);    //constrain is a predefine fn used to constrain(limit) our coordinates
+this.y=constrain(this.y, 0, height-scl);   // it is taking height n width automatically(can be tested on console)
+}
+
+this.show=()=>{
+    fill(255);
+    for(let i=0 ; i<this.tail.length ; i++){  //in this loop the tail of the snake will move with snake
+       rect(this.tail[i].x , this.tail[i].y , scl,scl);
+    }
+//fill(255);  //makes rect white
+for(let i=0; i<this.total; i++){
+    rect(this.tail[i].x, this.tail[i].y, scl, scl);
+}
+
+rect(this.x, this.y, scl,scl);   //rect of 10x10 size and this.x&y showing coordinates of x&y //snake(saanp)
+}
+
+
+=======
 function Snake(){  //it is a class or constructor function (class--> thats why name in capital)
                     //this function is working like a contructor of a class
 this.x=0;
@@ -66,4 +139,5 @@ rect(this.x, this.y, scl,scl);   //rect of 10x10 size and this.x&y showing coord
 }
 
 
+>>>>>>> d28b8c0aff777bfccb08b526c2dd40b39d7329b4
 }
